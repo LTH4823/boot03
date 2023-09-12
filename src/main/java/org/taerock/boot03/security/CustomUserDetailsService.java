340634268984ffca06c27cfc06d8261e2c2abe35
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2023-09-12 10:51:32 +0900

    update CustomUserDetailsService for checked db member login

--- src/main/java/org/taerock/boot03/security/CustomUserDetailsService.java
+++ src/main/java/org/taerock/boot03/security/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package org.taerock.boot03.security;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,6 +9,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.taerock.boot03.domain.Member;
+import org.taerock.boot03.repository.MemberRepository;
+
+import java.util.Optional;
 
 @Service
 @Log4j2
@@ -15,6 +20,9 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     //PasswordEncoder를 직접적으로 주입한 방식
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     public CustomUserDetailsService(){
         this.passwordEncoder = new BCryptPasswordEncoder();
@@ -26,6 +34,13 @@ public class CustomUserDetailsService implements UserDetailsService {
         log.info("=========================================");
         log.info("================login====================");
         log.info(username);
+
+        log.info(memberRepository.getWithRoles(username));
+        Optional<Member> resultDate = memberRepository.getWithRoles(username);
+
+        Member member = resultDate.orElseThrow(() -> new UsernameNotFoundException(username));
+
+        log.info(member);
 
         UserDetails result  = User.builder()
                 .username(username)
